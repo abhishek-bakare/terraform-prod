0177fac626c942e4e1011c0f6a78e44a42e8262a
abhishek-bakare <drushyaindia@gmail.com>
2026-06-12 18:35:03 +0530

    nothing

--- provider.tf
+++ provider.tf
@@ -19,20 +19,21 @@ terraform {
   }
 }
 
-variable "role_to_assume" {
-  description = "Used for provider role"
-  type        = string
-}
+#variable "role_to_assume" {
+#  description = "Used for provider role"
+#  type        = string
+#}
 
 provider "aws" {
   region = "us-east-1"
 
+  #as we already using OIDC roles in action so no need to use it here again using var, if you have different role for terraform then you can put here in variable as default
   # in prod we never use hardcoded access keys, we use role like below
-  assume_role {
-    #  # The ARN of the IAM role you want Terraform to assume
-    role_arn = var.role_to_assume
+  #assume_role {
+  #  # The ARN of the IAM role you want Terraform to assume
+  #  role_arn = var.role_to_assume
 
-    #  # Optional: A session name to identify who/what made the changes in AWS CloudTrail
-    session_name = "GitHubActions-TF"
-  }
+  #  # Optional: A session name to identify who/what made the changes in AWS CloudTrail
+  #  session_name = "GitHubActions-TF"
+  #}
 }
